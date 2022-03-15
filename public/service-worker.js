0c2fe702717c46272f6f0e68fcd3db4404426dab
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-03-14 22:20:01 -0400

    Scripts added to Heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
+  '/',
   './index.html',
   './css/styles.css',
   './js/index.js',
