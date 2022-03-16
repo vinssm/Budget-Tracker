d29c5dfa9bc2de81a0da881321b0e3052036b7be
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-03-15 20:19:35 -0400

    heroku

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"; 
 
 const app = express();
 
