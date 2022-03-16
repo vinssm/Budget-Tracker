9bebae035fa12333169aeda0adeeba12e7443371
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-03-15 20:19:35 -0400

    Added heroku for deployment

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,7 @@
 const APP_PREFIX = 'BudgetTracker';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-const FILES_TO_CACHE = [
-  '/',
+const FILES_TO_CACHE = [  
   './index.html',
   './css/styles.css',
   './js/index.js',
